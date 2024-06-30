b345ed8770b7b38d60d0efb235da7f95af9ae1f8
Shreesh Nautiyal <114166000+Shreesh09@users.noreply.github.com>
2024-06-30 17:28:02 +0530

    Update server.py

--- server/server.py
+++ server/server.py
@@ -95,6 +95,6 @@ def get_user_history():
 
     
 
-print('Check http://127.0.0.1:5000/')
+# print('Check http://127.0.0.1:5000/')
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port='8000')
