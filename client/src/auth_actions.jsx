64ec105649442e2814b6098a1a281a247c5e95d3
Shreesh <shreeshnautiyal3@gmail.com>
2024-07-06 00:24:20 +0530

    check

--- client/src/auth_actions.jsx
+++ client/src/auth_actions.jsx
@@ -2,6 +2,20 @@ import { redirect } from "react-router-dom";
 import { SERVER_URL } from "./keys";
 import axios from "axios";
 
+async function loginAction({request}) {
+  const formData = await request.formData();
+  try {
+    const username = formData.get('username');
+    const password = formData.get('password');
+    const response = await axios.post(`${SERVER_URL}/login`, {username, password})
+    sessionStorage.setItem('token', response.data.access_token);
+    sessionStorage.setItem('username', username)
+    return redirect('/home/dashboard/1');
+  } catch (e) {
+    console.log(e)
+    return "Invalid username or password"
+  }
+}
 
 async function registrationAction({request}) {
   const formData = await request.formData();
@@ -15,4 +29,4 @@ async function registrationAction({request}) {
   return redirect('/login');
 }
 
-export { registrationAction};
+export { registrationAction, loginAction};
